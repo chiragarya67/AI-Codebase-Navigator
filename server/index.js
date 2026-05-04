4143bf72a452fcb603a68bcd5b82c9f37710d9fd
chirag <chiragarya5678@gmail.com>
2026-05-04 12:22:13 +0530

    Update CORS frontend URL for development

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 
