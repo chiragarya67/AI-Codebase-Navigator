f85f2d61ab37bcb1bea73196aaadcd7388a4ea6d
chirag <chiragarya5678@gmail.com>
2026-05-04 12:14:26 +0530

    Update API baseURL to include '/api' path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-codebase-navigator-i5f3.onrender.com",
+  baseURL: "https://ai-codebase-navigator-i5f3.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
